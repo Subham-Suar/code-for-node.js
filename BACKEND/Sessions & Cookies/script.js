45de10bb5553a8e065a85cc1fe869f752dbbadc8
Subham-Suar <subhamsuar911609@gmail.com>
2026-03-06 23:10:38 +0530

    ejs and Dynamic routing

--- BACKEND/Sessions & Cookies/script.js
+++ BACKEND/Sessions & Cookies/script.js
@@ -1,12 +1,9 @@
-// const express = require('express')
-// const app =express();
-
-// app.use(express.json());
-// app.use(express.urlencoded({extended : true}));
-
 const express = require('express')
 const app =express();
 
 app.use(express.json());
-// app.use(express.urlencoded({extended : true}));
+app.use(express.urlencoded({extended : true}));
 
+
+app.use(express.json())
+app.use(express.urlencoded());
